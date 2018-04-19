33d6ca87e13cf79b122f3bfcd1c1de95a489c2c7
Eugênio Cabral <eugfcl@gmail.com>
2018-04-19 12:12:20 -0300

    Add ac3 initial implementation

--- heuristics.py
+++ heuristics.py
@@ -65,6 +65,7 @@ def ordered_domain_values(csp, assignment, variable):
 def random_domain_values(csp, assignment, variable):
     """ Método: Embaralhado (RND) """
     shuffle(csp.domains[variable])
+    return csp.domains[variable]
 
 def lcv(csp, assignment, variable):
     """ Método: Least-constraining-values (LCV) """
@@ -85,7 +86,7 @@ def dont_look_ahead(csp, assignment, variable, value, pruned):
     return True
 
 def forward_checking(csp, assignment, variable, value, pruned):
-    """ Aplica a inferencia de Checar Adiante """
+    """ Aplica a inferencia de Checagem Adiante """
     for N in csp.variable_neighbors[variable]:
         if N not in assignment:
             for n in csp.domains[N]:
@@ -96,3 +97,34 @@ def forward_checking(csp, assignment, variable, value, pruned):
             if not csp.domains[N]:
                 return False
     return True
+
+def ac3(csp, assignment, variable, value, pruned):
+    """ Aplica a inferencia de Consistencia de Arcos """
+    queue = [(neighbor, variable) for neighbor in csp.variable_neighbors[variable]]
+    
+    while queue:
+        neighbor, variable = queue.pop()
+        if _revise(csp, assignment, neighbor, variable, pruned):
+            if not csp.domains[neighbor]:
+                return False
+            for n in csp.variable_neighbors[neighbor]:
+                if n != variable:
+                    queue.append((n, neighbor))
+    return True
+
+def _revise(csp, assignment, neighbor, variable, pruned):
+    """Return true if we remove a value."""
+    revised = False
+    
+    for x in csp.domains[neighbor]:
+        csp.assign(assignment, neighbor, x)
+        for y in csp.domains[variable]:
+            csp.assign(assignment, variable, y)
+            if not csp.is_consistent(assignment):
+                for p in csp.prune(neighbor, x):
+                    pruned.append(p)
+                revised = True
+            csp.unassign(assignment, variable, y)
+        csp.unassign(assignment, neighbor, x)
+
+    return revised
